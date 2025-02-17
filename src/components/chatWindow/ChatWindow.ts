688a14ddf1cb6e793e20e793d4d465607bc1bacd
m3wqp <m3wqp@mail.ru>
2025-02-17 23:00:28 +0300

    Добавление компонента с WidjetModal при нажатии выпадает настраевыемый список

--- src/components/chatWindow/ChatWindow.ts
+++ src/components/chatWindow/ChatWindow.ts
@@ -1,6 +1,7 @@
 import Handlebars from 'handlebars';
 import template from './ChatWindow.hbs?raw';
 import './ChatWindow.scss';
+import { WidgetModal } from '../widjetModal/WidgetModal';
 
 interface Message {
   type: 'sent' | 'received';
@@ -19,5 +20,9 @@ export interface ChatWindowProps {
 
 export function ChatWindow({ partner, messages }: ChatWindowProps) {
   const compiledTemplate = Handlebars.compile(template);
-  return compiledTemplate({ partner, messages });
+  return compiledTemplate({
+    partner,
+    messages,
+    widgetModal: WidgetModal({ menuItems: ['Действие 1', 'Действие 2', 'Действие 3'] })
+  });
 }
